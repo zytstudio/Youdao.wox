f495f8448fd0903cbb76fb6e0e225068e545fd4e
ZYT <13716918961@qq.com>
2025-12-12 06:34:56 +0800

    Use new API

--- README.md
+++ README.md
@@ -6,7 +6,5 @@
 2. 无调用限制
 3. 可以获取发音
 
-
-~~# Youdao.wox
+# Youdao.wox
 A wox plugin using youdao api to translate input text. Python implement version from [Wox.Plugin.Youdao](https://github.com/qianlifeng/Wox.Plugin.Youdao), adding proxy support.
-~~
--- youdao.py
+++ youdao.py
@@ -2,149 +2,159 @@ import http.client
 import json
 import urllib
 import webbrowser
+import time
+import hashlib
+import uuid
+from configparser import ConfigParser
 
-from wox import Wox
-
-QUERY_URL = 'http://dict.youdao.com/search?q='
-EMPTY_RESULT = {
-    'Title': 'Start to translate between Chinese and English',
-    'SubTitle': 'Powered by youdao api, Python3.x only.',
-    'IcoPath': 'Img\\youdao.ico'
-}
-SERVER_DOWN = {
-    'Title': '网易在线翻译服务暂不可用',
-    'SubTitle': '请待服务恢复后再试',
-    'IcoPath': 'Img\\youdao.ico'
-}
-ERROR_INFO = {
-    "101": "缺少必填的参数，出现这个情况还可能是et的值和实际加密方式不对应",
-    "102": "不支持的语言类型",
-    "103": "翻译文本过长",
-    "104": "不支持的API类型",
-    "105": "不支持的签名类型",
-    "106": "不支持的响应类型",
-    "107": "不支持的传输加密类型",
-    "108": "appKey无效，注册账号，登录后台创建应用和实例并完成绑定，可获得应用ID和密钥等信息，其中应用ID就是appKey（注意不是应用密钥）",
-    "109": "batchLog格式不正确",
-    "110": "无相关服务的有效实例",
-    "111": "开发者账号无效",
-    "113": "q不能为空",
-    "201": "解密失败，可能为DES,BASE64,URLDecode的错误",
-    "202": "签名检验失败",
-    "203": "访问IP地址不在可访问IP列表",
-    "205": "请求的接口与选择的接入方式不一致",
-    "301": "辞典查询失败",
-    "302": "翻译查询失败",
-    "303": "服务端的其它异常",
-    "401": "账户已经欠费",
-    "411": "访问频率受限,请稍后访问",
-    "2005": "ext参数不对",
-    "2006": "不支持的voice",
-}
+from wox import Wox, WoxAPI
 
 
 class Main(Wox):
+    __HEADERS = {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    __EMPTY_RESULT = [{
+        'Title': 'Start to translate between Chinese and English',
+        'SubTitle': 'Powered by youdao api, Python3.x only.',
+        'IcoPath': 'img\\youdao.ico'
+    }]
+    __SERVER_DOWN = [{
+        'Title': '网易在线翻译服务暂不可用',
+        'SubTitle': '请待服务恢复后再试',
+        'IcoPath': 'img\\youdao.ico'
+    }]
+    __NETWORK_ERROR = [{
+        'Title': '网络请求失败',
+        'SubTitle': '请检查网络连接是否正常',
+        'IcoPath': 'img\\youdao.ico'
+    }]
+    __NO_KEY = [{
+        'Title': '未填写Key',
+        'SubTitle': '请在key.ini中的key中填写app_key与app_secret',
+        'IcoPath': 'img\\youdao.ico'
+    }]
 
-    def query(self, param):
-        result = []
+    def __init__(self):
+        self.__hash_object = hashlib.sha256()
+        self.__error_codes: dict = {}
+        self.__load_error_codes()
+        self.__app_key = ""
+        self.__app_secret = ""
+        self.__load_key()
+        Wox.__init__(self)
+
+    def __load_error_codes(self):
+        try:
+            with open('error_codes.json', 'r', encoding='utf-8') as file:
+                self.__error_codes = json.load(file)
+        except:
+            self.__error_codes = {}
+
+    def __load_key():
+        try:
+            conf = ConfigParser()
+            conf.read("key.ini")
+            self.__app_key = conf.get("key", "app_key")
+            self.__app_secret = conf.get("key", "app_secret")
+        except:
+            self.__app_key = ""
+            self.__app_secret = ""
+    
+    def query(self, param: str) -> list:
         q = param.strip()
         if not q:
-            return [EMPTY_RESULT]
+            return self.__EMPTY_RESULT
 
-        response = self.yd_api(q)
+        if not (self.__app_key and self.__app_secret):
+            return self.__NO_KEY
+        
+        response = self.__translate_api(q)
         if not response:
+            return self.__NETWORK_ERROR
+
+        error_code = response.get('errorCode', None)
+        if not error_code:
+            return self.__SERVER_DOWN
+        
+        if error_code != "0":
             return [{
-                'Title': '网络请求失败',
-                'SubTitle': '请检查网络连接是否正常',
-                'IcoPath': 'Img\\youdao.ico'
+                "Title": self.__error_codes.get(error_code, "未知错误"),
+                "SubTitle": "errorCode=%s" % error_code,
+                "IcoPath": "img\\youdao.ico"
             }]
-        errCode = response.get('errorCode', '')
-        if not errCode:
-            return [SERVER_DOWN]
-
-        if errCode != '0':
-            return [{
-                'Title': ERROR_INFO.get(errCode, '未知错误'),
-                'SubTitle': 'errorCode=%s' % errCode,
-                'IcoPath': 'Img\\youdao.ico'
-            }]
-
-        tSpeakUrl = response.get('tSpeakUrl', '')
-        translation = response.get('translation', [])
-        basic = response.get('basic', {})
-        web = response.get('web', [])
-
-        if translation:
-            result.append({
-                'Title': translation[0],
-                'SubTitle': '有道翻译',
-                'IcoPath': 'Img\\youdao.ico',
-                'JsonRPCAction': {
-                    'method': 'open_url',
-                    'parameters': [q, QUERY_URL]
-                }
-            })
-
-        if tSpeakUrl:
+        
+        translations = response.get('translation', [])
+        speak_url = response.get('tSpeakUrl', '')
+        web_dict = response.get('webdict', '')
+        
+        result: list = []
+        if translations:
+            for translation in translations:
+                result.append({
+                    'Title': translation,
+                    'SubTitle': '有道翻译',
+                    'IcoPath': 'img\\youdao.ico',
+                    'JsonRPCAction': {
+                        'method': 'open_url',
+                        'parameters': [web_dict.get('url', '')]
+                    }
+                })
+        
+        if speak_url:
             result.append({
                 'Title': '获取发音',
                 'SubTitle': '点击可跳转 - 有道翻译',
-                'IcoPath': 'Img\\youdao.ico',
+                'IcoPath': 'img\\youdao.ico',
                 'JsonRPCAction': {
                     'method': 'open_url',
-                    'parameters': [tSpeakUrl]
+                    'parameters': [speak_url]
                 }
             })
-        if basic:
-            for i in basic['explains']:
-                result.append({
-                    'Title': i,
-                    'SubTitle': '{} - 基本词典'.format(response.get('query', '')),
-                    'IcoPath': 'Img\\youdao.ico',
-                    'JsonRPCAction': {
-                        'method': 'open_url',
-                        'parameters': [q, QUERY_URL]
-                    }
-                })
-        if web:
-            for i in web:
-                result.append({
-                    'Title': ','.join(i['value']),
-                    'SubTitle': '{} - 网络释义'.format(i['key']),
-                    'IcoPath': 'Img\\youdao.ico',
-                    'JsonRPCAction': {
-                        'method': 'open_url',
-                        'parameters': [q, QUERY_URL]
-                    }
-                })
         return result
 
-    def open_url(self, query, url=None):
+    def open_url(self, url: str) -> str:
         if url:
-            webbrowser.open(url + query)
-        else:
-            webbrowser.open(query)
+            webbrowser.open(url)
 
-    @staticmethod
-    def yd_api(q):
-        payload = "q={}&from=Auto&to=Auto".format(urllib.parse.quote(q))
-        headers = {
-            'Content-Type': "application/x-www-form-urlencoded",
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
-            'Cache-Control': "no-cache"
+    def __get_sign(self, q: str, salt: str, cur_time: str) -> str:
+        input_str = ""
+        if len(q) <= 20:
+            input_str = q
+        else:
+            input_str = q[:10] + str(len(q)) + q[-10:]
+        self.__hash_object.update((self.__app_key + input_str + salt + cur_time + self.__app_secret).encode('utf-8'))
+        return str(self.__hash_object.hexdigest())
+        
+    def __translate_api(self, q: str):
+        cur_time = str(int(time.time()))
+        salt = str(uuid.uuid1())
+        sign = self.__get_sign(q, salt, cur_time)
+        data = {
+            "q": q,
+            "from": "auto",
+            "to": "zh-CHS",
+            "appKey": self.__app_key,
+            "salt": salt,
+            "sign": sign,
+            "signType": "v3",
+            "curtime": cur_time
         }
+        payload = urllib.parse.urlencode(data).encode('utf-8')
+
         try:
-            conn = http.client.HTTPSConnection("aidemo.youdao.com")
-            conn.request("POST", "/trans", payload, headers)
-            res = conn.getresponse()
-            if res.code == 200:
-                return json.loads(res.read().decode("utf-8"))
-        except Exception:
-            pass
+            conn = http.client.HTTPSConnection("openapi.youdao.com")
+            conn.request("POST", "/api", body=payload, headers=self.__HEADERS)
+            response = conn.getresponse()
+            if response.code == 200:
+                return json.loads(response.read().decode("utf-8"))
+            return None
+        except:
+            return None
         finally:
             if conn:
                 conn.close()
-
+                
     def __get_proxies(self):
         proxies = {}
         if self.proxy and self.proxy.get("enabled") and self.proxy.get("server"):
